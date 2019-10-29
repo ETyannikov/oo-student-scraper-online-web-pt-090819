1335348d94cac60f7e382e269736e80dbf18ee91
Eddy Tyannikov <etyannikov@gmail.com>
2019-10-29 23:59:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,47 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
-  def self.scrape_index_page(index_url)
+  
+  def self.scrape_index_page(html)
+    site = Nokogiri::HTML(open(html))
     
+    students = []
+    
+    #student: "div.student-card"
+    #name: student.css("a div.card-text-container h4").text
+    #location: student.css("a div.card-text-container p").text
+    #profile_url: student.css("a").attribute("href").value
+    
+    site.css("div.student-card").each do |profile| 
+    student = {
+      :name => profile.css("a div.card-text-container h4").text,
+      :location => profile.css("a div.card-text-container p").text,
+      :profile_url => profile.css("a").attribute("href").value
+    }
+    students << student
+  end
+  students
   end
 
-  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page
+    site = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html"))
     
+    student = {}
+    
+    student[:profile_quote] = site.css("div.profile-quote").text
+    student[:bio] = site.css("div.description-holder p").text
+    
+    site.css("div.social-icon-container").each do |url|
+      if url.include? "twitter"
+        student[:twitter] = "test"
+      end
+    end #each loop
+    
+    binding.pry
   end
 
+  scrape_profile_page
 end
 
